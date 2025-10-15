761211d17dd79f8af93f0eeb14f3946be0abbbfb
Lexi Wheadon <lexiwheadon@hotmail.com>
2025-10-15 18:19:24 +0100

    Basic functionality working

--- tkinterApp.py
+++ tkinterApp.py
@@ -1,15 +1,34 @@
 import tkinter as tk
 import subprocess
+import os
 
 root = tk.Tk()
 root.title("Installer")
 root.focus_force()
 root.minsize(300, 200)
 
+def badPopup():
+    popup = tk.Tk()
+    popup.wm_title("!")
+    popup.minsize(200, 100)
+    tk.Message(popup, text="Installation failed. Please try again.").pack()
+
+def goodPopup():
+    popup = tk.Tk()
+    popup.wm_title("!")
+    popup.minsize(200, 100)
+    tk.Message(popup, text="Installation successful!").pack()
+
 def download():
-    try: subprocess.run("winget install Brave.Brave --silent", check=True)
+    tk.Message(root, text="Installing... Please wait.").pack()
+    root.update()
+    try: subprocess.run("winget install Brave.Brave --silent --accept-package-agreements --accept-source-agreements", check=True)
     except:
-        print("Error during installation")
+        badPopup()
+        return
+    else:
+        goodPopup()
+        root.destroy()
 
 button = tk.Button(root, text="Download", command=download)
 button.pack(pady=20)
